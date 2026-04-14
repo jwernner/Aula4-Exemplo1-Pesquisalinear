91e739203969d308547e40623014e30c966c6392
jwernner <jorge.werner@gmail.com>
2026-04-14 15:47:24 -0300

    commit 1

---
+++ src/aula4/exemplo1/pesquisalinear/Aula4Exemplo1Pesquisalinear.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula4.exemplo1.pesquisalinear;
+
+/**
+ *
+ * @author jwern
+ */
+public class Aula4Exemplo1Pesquisalinear {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
