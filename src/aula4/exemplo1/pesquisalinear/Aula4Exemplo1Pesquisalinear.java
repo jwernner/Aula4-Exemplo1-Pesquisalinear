258911984615c4c1c3b34de5f0f514552f17ff59
jwernner <jorge.werner@gmail.com>
2026-04-14 15:50:54 -0300

    motagem do projeto

--- src/aula4/exemplo1/pesquisalinear/Aula4Exemplo1Pesquisalinear.java
+++ src/aula4/exemplo1/pesquisalinear/Aula4Exemplo1Pesquisalinear.java
@@ -4,6 +4,8 @@
  */
 package aula4.exemplo1.pesquisalinear;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jwern
@@ -13,8 +15,31 @@ public class Aula4Exemplo1Pesquisalinear {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String args[]) {
+
+        int i, flag;
+        int numElementos = Integer.parseInt(
+            JOptionPane.showInputDialog("Digite o número de pessoas a ser cadastrado")
+        );
+
+        String vetorPesquisado[] = new String[numElementos];
+
+        for (i = 0; i < numElementos; i++) {
+            vetorPesquisado[i] = JOptionPane.showInputDialog("Digite o nome para cadastro");
+        }
+
+        String elementoProcurado = JOptionPane.showInputDialog("Digite o nome a ser procurado");
+        flag = 0;
+
+        for (i = 0; i < numElementos; i++) {
+            if (vetorPesquisado[i].equalsIgnoreCase(elementoProcurado)) {
+                JOptionPane.showMessageDialog(null, "O valor procurado foi encontrado na posição " + i);
+                flag = 1;
+            }
+        }
+
+        if (flag == 0) {
+            JOptionPane.showMessageDialog(null, "O nome não foi encontrado.");
+        }
     }
-    
 }
